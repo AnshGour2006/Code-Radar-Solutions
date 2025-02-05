0414bb2a69f31c24cb6d1ff9013f8f7a8172f704
Ansh Gour <ansh0264.becse24@chitkara.edu.in>
2025-02-05 12:11:40 +0530

    Solution to Logical AND Operation

---
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+   int a,b;
+   scanf("%d %d",&x,&y);
+   if(a<0 && b<0){
+    printf("True\n");
+   }
+   else{
+    printf("False\n");
+   }
+}
