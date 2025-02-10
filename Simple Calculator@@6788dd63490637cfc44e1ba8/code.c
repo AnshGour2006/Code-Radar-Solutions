57b5bf1f5bf261511e8d3a7d31ad35c195ccfb25
Ansh Gour <ansh0264.becse24@chitkara.edu.in>
2025-02-10 09:49:51 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -14,7 +14,7 @@ int main() {
         if (b != 0) {
             printf("%d\n", a / b);
         } else {
-            printf("Error: Division by zero.\n");
+            printf("error.\");
         }
     } else {
         printf("error");
