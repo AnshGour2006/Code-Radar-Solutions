63dd730c385f9e5029b66c3ab06f60aa8a7cc124
Ansh Gour <ansh0264.becse24@chitkara.edu.in>
2025-02-10 09:15:10 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+   char grade;
+   scanf("%d",&grade);
+   if(grade=='A'){
+    printf("Excellent");
+   }
+   else if(grade=='B'){
+    printf("Good");
+   }
+   else if(grade=='C'){
+    printf("Average");
+   }
+   else if(grade=='D'){
+    printf("Below");
+   }
+   else{
+    printf("Fail");
+   }
+    return 0;
+}
