124e1592a122470a2eaef00643f41d33aa30e091
Ansh Gour <ansh0264.becse24@chitkara.edu.in>
2025-02-11 12:06:19 +0530

    Solution to Check Month Days

---
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int num;
+    sacnf("%d",&num);
+    if(num==1,3,5,7,8,10,12){
+        printf("31\n");
+    }
+}
