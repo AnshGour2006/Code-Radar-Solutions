f50b9813a7323001a2cc0d0db1cc4b4d2e501f32
Ansh Gour <ansh0264.becse24@chitkara.edu.in>
2025-02-11 12:15:26 +0530

    Solution to Check Divisibility

---
+++ Check Divisibility@@6788dd63490637cfc44e1b91/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+   int num;
+   sacnf("%d",&num);
+   if(num%5==0 && num%11==0){
+    printf("Divisible");
+   }
+   else{
+    printf("Not Divisible");
+   }
+    return 0;
+}
