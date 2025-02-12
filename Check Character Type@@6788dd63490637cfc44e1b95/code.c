d9ccbf57aed3f3505efbf0a1f85d2c3a0fe955d3
Ansh Gour <ansh0264.becse24@chitkara.edu.in>
2025-02-12 11:36:58 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+        printf("Vowel\n");
+    }
+    else{
+        printf("consonant\n");
+    }
+}
