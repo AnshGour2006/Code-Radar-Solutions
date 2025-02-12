46f5e4b57615951d1ec39110581f524d37cf0b02
Ansh Gour <ansh0264.becse24@chitkara.edu.in>
2025-02-12 11:27:27 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -4,14 +4,35 @@ char* welcome() {
     return "Welcome to Code Radar!";
 }
 
-int main() {
-   int a, b, c;
-   scanf("%d %d %d", &a, &b, &c);  
-   if (a * a == b * b + c * c) {   
-       printf("Valid\n");
-   } else {
-       printf("Invalid\n");
-   }
+// Function to check if a, b, c form a right-angled triangle
+int is_right_triangle(int a, int b, int c) {
+    // Find the largest side (hypotenuse)
+    int max = a, side1 = b, side2 = c;
+    if (b > max) {
+        max = b;
+        side1 = a;
+        side2 = c;
+    }
+    if (c > max) {
+        max = c;
+        side1 = a;
+        side2 = b;
+    }
+    
+    // Check Pythagorean theorem
+    return (max * max == side1 * side1 + side2 * side2);
+}
 
-   return 0;
+int main() {
+    int a, b, c;
+    printf("Enter three sides of the triangle: ");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    if (is_right_triangle(a, b, c)) {
+        printf("Valid\n");
+    } else {
+        printf("Invalid\n");
+    }
+    
+    return 0;
 }
