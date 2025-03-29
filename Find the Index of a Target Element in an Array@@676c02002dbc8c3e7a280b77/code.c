19b6021b4e8795a0ed667a6f7c4488941417f8a4
Ansh Gour <ansh0264.becse24@chitkara.edu.in>
2025-03-29 11:00:02 +0530

    Solution to Find the Index of a Target Element in an Array

--- Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
+++ Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
@@ -1,28 +1,40 @@
-#include<stdio.h>
-int target_index(int arr[],int n,int x){
-    for(int i=0;i<n;i++){
-        if(arr[i]==x){
-            return i;
-        }
-        else{
-            return -1;
+#include <stdio.h>
+
+int target_index(int arr[], int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            return i; // Return the index
         }
     }
+    return -1; // If element is not found
 }
-int main(){
-int n,x;
-scanf("%d",&n);
-int arr[n];
-for(int i=0;i<n;i++){
-    scanf("%d\\n",&arr[i]);
-}
-scanf("%d\n",&x);
-int index=target_index(arr,n,x);
-if(index!= -1){
-    printf("%d",index);
-}
-else{
-    printf("-1\n");
-}
-return 0;
+
+int main() {
+    int n, x;
+    
+    // Input array size
+    scanf("%d", &n);
+
+    int arr[n]; // Declare array
+    
+    // Input array elements
+ 
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input target element
+
+    scanf("%d", &x);
+
+    // Call the function
+    int index = target_index(arr, n, x);
+    
+    if (index != -1) {
+        printf("%d\n", index);
+    } else {
+        printf("-1\n");
+    }
+    
+    return 0;
 }
