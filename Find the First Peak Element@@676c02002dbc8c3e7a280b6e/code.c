543d2fb8bbd09b7f1ada3e00331bd331e3602db7
Ansh Gour <ansh0264.becse24@chitkara.edu.in>
2025-04-09 12:10:19 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,42 +1,24 @@
 #include<stdio.h>
+int firstPeak(int arr[],int n){
+    int num=0;
+    for(int i=0;i<n-2;i++){
+        if(arr[i]<arr[i+1] && arr[i+1]>arr[i-2])
+        num=arr[i];
 
-int peak(int arr[], int n) {
-    // If the array has only one element, that element is the peak.
-    if (n == 1) {
-        return arr[0];
     }
-
-    // Check if the first element is a peak
-    if (arr[0] >= arr[1]) {
-        return arr[0];
-    }
-
-    // Check if the last element is a peak
-    if (arr[n - 1] >= arr[n - 2]) {
-        return arr[n - 1];
-    }
-
-    // Check the middle elements for a peak
-    for (int i = 1; i < n - 1; i++) {
-        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
-            return arr[i];
-        }
-    }
-
-    return -1;  // Return -1 if no peak is found (shouldn't happen in most cases)
+    return num;
 }
 
-int main() {
+
+
+
+int main(){
     int n;
-    scanf("%d", &n);
+    scanf("%d",&n)
     int arr[n];
-    
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-    
-    int result = peak(arr, n);
-    printf("%d\n",result);
-    
-    return 0;
+  int result=firstPeak(arr,n);
+  printf("%d",result);
 }
