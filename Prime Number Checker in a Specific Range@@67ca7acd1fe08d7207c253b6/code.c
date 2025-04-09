ac4d51d2c68526751a7bfd2aad637cc9ba8c69b7
Ansh Gour <ansh0264.becse24@chitkara.edu.in>
2025-04-09 11:36:22 +0530

    Solution to Prime Number Checker in a Specific Range

--- Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
+++ Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
@@ -1,16 +1,18 @@
-// Your code here...
- void PrimesInRange(int arr[],int num;){
-    int count=0;
-    for(int i=0;i<num;i++){
-        if(num%i!=0){
-            count++;
+int isPrime(int num) {
+    if (num < 2) return 0; // 0 and 1 are not prime
+
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0)
+            return 0; // Not prime if divisible
+    }
+    return 1; // Prime if no divisor found
+}
+
+// Function to print all prime numbers up to 'num'
+void printPrimesInRange(int num) {
+    for (int i = 2; i <= num; i++) {
+        if (isPrime(i)) {          // Use helper function to check
+            printf("%d ", i);      // Print if prime
         }
     }
- }
- void printPrimesInRange(int arr[],int num){
-    for(int i=0;i<num;i++){
-        if(is(prime)){
-            printf("%d ",i);
-        }
-    }
- }
+}
